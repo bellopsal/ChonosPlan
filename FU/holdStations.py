48d0d972b22fef6d29e90e3b95348c68883cf642
bellopsal <bellopsal@alum.us.es>
2024-03-11 08:48:54 +0100

    Update HoldStations.py

--- FU/holdStations.py
+++ FU/holdStations.py
@@ -64,7 +64,7 @@ class HS:
         self.n = n_hs
         self.csv = "hs.csv"
 
-        with open("ss.csv", "w") as f:
+        with open("hs.csv", "w") as f:
             write = csv.writer(f)
             fields = ["SS", "bitMux","RP1","RP2","FU1", "FU2", "value1","value1"]
             rows = [[str(i), self.l_hs[i].bitMux, self.l_hs[i].RP1, self.l_hs[i].RP2, self.l_hs[i].FU1, self.l_hs[i].FU2, self.l_hs[i].value1,self.l_hs[i].value2] for i in range(n_hs - 1, -1, -1)]
@@ -89,130 +89,33 @@ class HS:
     def one_clock_cycle(self,CDB,n_ss, n_pile):
         exitHS = []
         for e in self.l_hs:
-            exitHS.append(e.one_clock_cycle(CDB))
+            exitHS.append(e.one_clock_cycle(CDB, n_ss, n_pile))
 
         return exitHS
 
 
 
     def get(self,i):
-        return self.l_ss[i]
+        return self.l_hs[i]
 
-    def update_i(self, i, bitMux, FU1, FU2, RP, value, type_operation, inv, inm= None):
-        self.l_ss[i].bitMux = bitMux
-        self.l_ss[i].FU1 = FU1
-        self.l_ss[i].FU2 = FU2
-        self.l_ss[i].RP = RP
-        self.l_ss[i].value = value
-        self.l_ss[i].inv = inv
-        self.l_ss[i].inm = inm
-        self.l_ss[i].type_operation = type_operation
+    def update_i(self, i, bitMux, FU1, FU2, RP1,RP2, value1,value2, type_operation, inv,bitUse, inm= None):
+        self.l_hs[i].bitMux = bitMux
+        self.l_hs[i].FU1 = FU1
+        self.l_hs[i].FU2 = FU2
+        self.l_hs[i].RP1 = RP1
+        self.l_hs[i].RP2 = RP2
+        self.l_hs[i].value1 = value1
+        self.l_hs[i].value2 = value2
+        self.l_hs[i].inv = inv
+        self.l_hs[i].inm = inm
+        self.l_hs[i].type_operation = type_operation
 
 
-        with open("ss.csv", "a") as f:
+        with open("hs.csv", "a") as f:
             write = csv.writer(f)
             fields = ["SS", "bitMux", "RP", "FU1", "FU2", "value"]
-            rows = [[str(i), self.l_ss[i].bitMux, self.l_ss[i].RP, self.l_ss[i].FU1, self.l_ss[i].FU2, self.l_ss[i].value] for i
+            rows = [[str(i), self.l_hs[i].bitMux, self.l_hs[i].RP1,self.l_hs[i].RP2, self.l_hs[i].FU1, self.l_hs[i].FU2, self.l_hs[i].value1, self.l_hs[i].value2] for i
                     in range(self.n - 1, -1, -1)]
             write.writerow(fields)
             write.writerows(rows)
 
-
-class PileElement:
-    def __init__(self):
-        self.value = None
-        self.fu = None
-        #self.bitUse = -1
-        self.RP = -1
-
-    def __str__(self):
-        return f"     {self.RP} | {self.fu} | {self.value}"
-
-
-    def __int__(self, value, fu,RP):
-        self.value = value
-        self.fu = fu
-        #self.bitUse = -1
-        self.RP = RP
-
-    def one_clock_cycle(self, CDB):
-        if self.RP == 1:
-            self.RP = -1
-            self.value = CDBhelper(self.fu,CDB)
-        elif self.RP > 1:
-            self.RP = self.RP - 1
-
-    def setValue(self, value):
-        self.value = value
-
-    def setFu(self, fu):
-        self.fu = fu
-
-    def setRP(self, RP):
-        self.RP = RP
-
-
-    def getValue(self):
-        return self.value
-
-    def getFU(self):
-        return self.fu
-
-    def getRP(self):
-        return self.RP
-
-
-
-class Pile:
-    def __init__(self, size):
-        self.pile = [PileElement() for i in range(size)]
-        self.n = size
-        self.csv = "pile.csv"
-
-        with open("pile.csv", "w") as f:
-            write = csv.writer(f)
-            fields = ["Element", "RP", "FU",  "value"]
-            rows = [[str(i), self.pile[i].RP, self.pile[i].fu,self.pile[i].value] for i
-                    in range(self.n - 1, -1, -1)]
-            write.writerow(fields)
-            write.writerows(rows)
-
-    def __str__(self):
-        res = ""
-        for i in range(self.n):
-            txt = "\n" + f"Pile{i} "+ str(self.pile[i])
-            res = txt + res
-        res = "      RP | FU |value" + res
-        return res
-
-    def get(self,i):
-        return self.pile[i]
-
-
-    def one_clock_cycle(self, CDB, bitMux, FU2):
-        if bitMux == 2:
-            self.pile[1].value = CDBhelper(FU2, CDB)
-            self.pile[1].RP = -1
-
-        for element in self.pile:
-            element.one_clock_cycle(CDB)
-
-        self.pile.append(PileElement())
-        res = self.pile[0]
-        self.pile.pop(0)
-
-        return res
-
-    # def update(self, CDB):
-    # #     for element in self.pile:
-    # #         if element.RP == 0:
-    # #             element.value = CDB.get(FU2)
-    # #             element.RP = -1
-    #
-    #     with open(self.csv, "a") as f:
-    #         write = csv.writer(f)
-    #         fields = ["Element", "RP", "FU",  "value"]
-    #         rows = [[str(i), self.pile[i].RP, self.pile[i].fu,self.pile[i].value] for i
-    #                 in range(self.n - 1, -1, -1)]
-    #         write.writerow(fields)
-    #         write.writerows(rows)
